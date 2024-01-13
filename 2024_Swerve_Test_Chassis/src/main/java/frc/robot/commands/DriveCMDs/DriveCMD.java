3620255f04effe3481e74a578ed681f7ad3d76bc
aryachaudry <75314990+Gavin32323@users.noreply.github.com>
2024-01-13 16:44:45 -0500

    BASE

---
+++ 2024_Swerve_Test_Chassis/src/main/java/frc/robot/commands/DriveCMDs/DriveCMD.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.DriveCMDs;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.DriveTrain;
+
+public class DriveCMD extends Command {
+ 
+  private boolean fieldOriented;
+    
+  private DriveTrain drive;
+
+  private CommandXboxController drivecontroller;
+
+  public DriveCMD(CommandXboxController controller, boolean fieldOriented, DriveTrain drive) {
+      this.drive = drive;
+      addRequirements(drive);
+
+      this.fieldOriented = fieldOriented;
+      this.drivecontroller = controller;
+  }
+
+  @Override
+  public void execute() {
+      drive.drive(
+          MathUtil.applyDeadband(drivecontroller.getLeftY(), 0.3),
+          MathUtil.applyDeadband(drivecontroller.getLeftX(), 0.3),
+          MathUtil.applyDeadband(-drivecontroller.getRightX(), 0.3),
+          fieldOriented);
+  }
+}
